b331f25f358f69e3bab54943d3e307f6e044e304
GamerPixelDev <agamerpixel@gmail.com>
2025-10-23 10:07:47 +0200

    Se actualiza el archivo logger_config.py para implementar los diferentes handlers

--- gestion_fichas/logger_config.py
+++ gestion_fichas/logger_config.py
@@ -2,6 +2,43 @@ import logging
 import os
 from datetime import datetime
 
+#=== Configuración de carpetas ===
+LOG_DIR = os.path.join(os.path.dirname(__file__), "..", "logs")
+os.makedirs(LOG_DIR, exist_ok = True)
+
+#=== Rutas de los archivos de log ===
+APP_LOG_FILE = os.path.join(LOG_DIR, "app.log")
+ERROR_LOG_FILE = os.path.join(LOG_DIR, "errors.log")
+USER_LOG_FILE = os.path.join(LOG_DIR, "user_actions.log")
+
+#=== Configuración general del logger ===
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+#--- Handler principal (mensajes generales) ---
+app_handler = logging.FileHandler(APP_LOG_FILE, encoding="utf-8")
+app_handler.setLevel(logging.INFO)
+app_handler.setFormatter(formatter)
+
+#--- Handler para errores ---
+error_handler = logging.FileHandler(ERROR_LOG_FILE, encoding="utf-8")
+error_handler.setLevel(logging.ERROR)
+error_handler.setFormatter(formatter)
+
+#--- Handler para acciones del usuario ---
+user_handler = logging.FileHandler(USER_LOG_FILE, encoding="utf-8")
+user_handler.setLevel(logging.INFO)
+user_handler.setFormatter(formatter)
+
+#=== Logger principal ===
+logger = logging.getLogger("gestion_fichas")
+logger.setLevel(logging.INFO)
+
+#Para evitare duplicidades de handlers
+if not logger.handlers:
+    logger.addHandler(app_handler)
+    logger.addHandler(error_handler)
+    logger.addHandler(user_handler)
+
 #Carpeta donde se guardam los logs
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOG_DIR = os.path.join(BASE_DIR, "logs")
